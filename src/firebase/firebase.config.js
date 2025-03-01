744a50956bf766e3314a6bf2bf04235ebda76c94
Alal Hosen <alalhosen1992620120@gmail.com>
2025-03-01 23:57:00 +0600

    create header component

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDabj7dw-iFkg7RCwnigfCK-qBH_3tuykw",
+  authDomain: "uttor--mondol-para-jame-mosjid.firebaseapp.com",
+  projectId: "uttor--mondol-para-jame-mosjid",
+  storageBucket: "uttor--mondol-para-jame-mosjid.firebasestorage.app",
+  messagingSenderId: "52409772877",
+  appId: "1:52409772877:web:d320d157c4556f8defa504"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
