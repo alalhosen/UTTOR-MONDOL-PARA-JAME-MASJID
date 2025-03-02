9670f823feff7484d21cc5723173cef0dd935dad
Alal Hosen <alalhosen1992620120@gmail.com>
2025-03-02 15:48:50 +0600

    set & update Navbar

---
+++ src/components/Connection.jsx
@@ -0,0 +1,9 @@
+const Connection = () => {
+  return (
+    <div>
+      <p>Connection</p>
+    </div>
+  );
+};
+
+export default Connection;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Root } from "postcss";
+import Root from "./components/Root.jsx";
+import Connection from "./components/Connection.jsx";
+import Gallery from "./components/Gallery.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    children:[
+      {
+        path: "/gallery",
+        element: <Gallery></Gallery>
+      },
+      {
+        path: "/connection",
+        element: <Connection></Connection>
+      }
+    ]
   },
 ]);
 
