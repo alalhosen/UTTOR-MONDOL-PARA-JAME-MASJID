f15181df39adf0033b8096f8ab6929acea614e1a
Alal Hosen <alalhosen1992620120@gmail.com>
2025-03-03 07:33:15 -0600

    list of name in romadan

--- src/components/Ramadan.jsx
+++ src/components/Ramadan.jsx
@@ -5,22 +5,108 @@ const Ramadan = () => {
             <p className="text-3xl bg-lime-400 text-center font-extrabold text-orange-500 p-4 mx-5 rounded-se-full">মসজিদে ইফতারের নামের তালিকাঃ</p>
         </div>
       <div className="overflow-x-auto">
-        <table className="table">
+        <table className="table text-center">
           {/* head */}
           <thead>
-            <tr className="text-xl font-extrabold">
-              <th>রমজান</th>
-              <th>ইংরেজি তারিখ</th>
-              <th>নাম</th>
+            <tr>
+              <th className="text-2xl text-lime-600">রমজান</th>
+              <th className="text-2xl text-lime-600">ইংরেজি তারিখ</th>
+              <th className="text-2xl text-lime-600">নাম</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
             <tr className="font-bold">
-              <th>১</th>
+              <th>১ রমজান</th>
               <td>২ মার্চ</td>
-              <td>মোঃ নিজামুদ্দিন</td>
+              <td>মোঃ নিজামউদ্দিন</td>
             </tr>
+            <tr className="font-bold">
+              <th>২ রমজান</th>
+              <td>৩ মার্চ</td>
+              <td>মোঃ শামসুল হক</td>
+            </tr>
+            <tr className="font-bold">
+              <th>৩ রমজান</th>
+              <td>৪ মার্চ</td>
+              <td>মোঃ ইসারুল হক</td>
+            </tr>
+            <tr className="font-bold">
+              <th>৪ রমজান</th>
+              <td>৫ মার্চ</td>
+              <td>মোঃ আমজাদ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>৫ রমজান</th>
+              <td>৬ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>৬ রমজান</th>
+              <td>৭ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>৭ রমজান</th>
+              <td>৮ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>৮ রমজান</th>
+              <td>৯ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>৯ রমজান</th>
+              <td>১০ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>১০ রমজান</th>
+              <td>১১ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>১১ রমজান</th>
+              <td>১২ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>১২ রমজান</th>
+              <td>১৩ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>১৩ রমজান</th>
+              <td>১৪ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>১৪ রমজান</th>
+              <td>১৫ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>১৫ রমজান</th>
+              <td>১৬ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>১৬ রমজান</th>
+              <td>১৭ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>১৭ রমজান</th>
+              <td>১৮ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+            <tr className="font-bold">
+              <th>১৮ রমজান</th>
+              <td>১৯ মার্চ</td>
+              <td>মোঃ </td>
+            </tr>
+           
           </tbody>
         </table>
       </div>
