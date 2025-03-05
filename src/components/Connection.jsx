cf66757c5c1037ab5f2c00e8a8473362505a7ca7
Alal Hosen <alalhosen1992620120@gmail.com>
2025-03-05 11:46:35 -0600

    emam saheb add

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -6,7 +6,7 @@ const Connection = () => {
         src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
         className="max-w- rounded-full shadow-xl" />
       <div>
-        <h1 className="text-5xl font-bold">Box Office News!</h1>
+        <h1 className="text-5xl font-bold">ইমাম</h1>
         <p className="py-6">
           Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
           quasi. In deleniti eaque aut repudiandae et a id nisi.
