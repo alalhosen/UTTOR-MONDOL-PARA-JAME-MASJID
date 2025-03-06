5304263ca747559eaa54e1a0e67c6688baeb73ff
Alal Hosen <alalhosen1992620120@gmail.com>
2025-03-06 11:56:15 -0600

    bg add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -85,7 +85,7 @@ const Home = () => {
               src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
               className="max-w-sm rounded-lg shadow-2xl"
             />
-            <div>
+            <div className="bg-lime-400">
               <h1 className="text-5xl font-bold">Box Office News!</h1>
               <p className="py-6">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
