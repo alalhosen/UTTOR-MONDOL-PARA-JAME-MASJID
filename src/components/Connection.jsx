249f3d5a9aa31ae2af1c8e426deb52452bcc62f0
Alal Hosen <alalhosen1992620120@gmail.com>
2025-03-19 19:41:55 -0600

    team memnber

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -1,6 +1,7 @@
 const Connection = () => {
   return (
     <div>
+      {/* emam  emam  emam  emam  emam  emam  emam  emam   */}
       <div
         className="hero min-h-screen"
         style={{
@@ -21,6 +22,18 @@ const Connection = () => {
           </div>
         </div>
       </div>
+      <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">মসজিস কমিটির সদস্যবৃন্দ</h1>
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
 
     </div>
   );
