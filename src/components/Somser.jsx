116a41661d93539acd80f0cbd01395ab7231e4a6
Alal Hosen <alalhosen1992620120@gmail.com>
2025-03-29 15:32:38 +0600

    cow death

--- src/components/Somser.jsx
+++ src/components/Somser.jsx
@@ -1,7 +1,18 @@
 const Somser = () => {
   return (
     <div>
-      <p></p>
+      <div>
+        <p className="text-xl font-extrabold underline"></p>
+        <p></p>
+      </div>
+      <div>
+        <p className="text-xl font-extrabold underline">২৩ মার্চ, ২০২৫</p>
+        <p>২৩ মার্চ ২২শে রমজান রবিবার বিকালে তাজকিরা, সমসের ও তার মা হালেমর ঝগড়া হলে হালেমা ঐদিন ওদের ইফতারি খায়নি। ... থেকে জার্শি গরুর জ্বর হয়। ... থাকে ... দিন গত মধ্য রাতে মারা যায়। মাটি দেয় বাড়ির পশ্চিমে মুনসুরের টিউবওয়েলের পাশের জমিতে।</p>
+      </div>
+      <div>
+        <p className="text-xl font-extrabold underline"></p>
+        <p></p>
+      </div>
     </div>
   );
 };
