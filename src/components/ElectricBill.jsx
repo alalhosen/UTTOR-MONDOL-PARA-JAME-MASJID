be39b9d5c849644ae5ab8dfc05000a5892516680
Alal Hosen <alalhosen1992620120@gmail.com>
2025-04-25 21:47:09 +0600

    headline add

--- src/components/ElectricBill.jsx
+++ src/components/ElectricBill.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const ElectricBill = () => {
     return (
         <div>
-            
+            <div>
+                <p>মাসিক বিদ্যুৎ বিল পরিশোধকারী ব্যক্তিগণের নামের তালিকাঃ-</p>
+            </div>
         </div>
     );
 };
